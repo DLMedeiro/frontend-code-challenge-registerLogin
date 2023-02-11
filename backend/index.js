236ae676447e1aa7e017620cf7dc91b17fa60634
DanielleM <dlmedeiro@gmail.com>
2023-02-11 10:57:47 -0800

    add cors

--- backend/index.js
+++ backend/index.js
@@ -5,11 +5,13 @@ var favicon = require("serve-favicon");
 var cookieParser = require("cookie-parser");
 var bodyParser = require("body-parser");
 var logger = require("./lib/logger");
+const cors = require("cors");
 
 var users = require("./routes/users");
 
 var app = express();
 var log = logger(app);
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -17,6 +19,16 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/users", users);
+
+// Add Access Control Allow Origin headers
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http:localhost:3001/");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
